5d76c7927b5a3dc2b79ce80a6ae4c2ae9f9b56a0
Divyajeet Singh <knightt1821@gmail.com>
2022-08-25 00:10:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ import pyautogui
 import time
 
 
-start: int = int(input("Enter the starting countdown: "))
-interval: int = int(input("Enter the interval between each word: "))
+def read() -> str:
+    """reads and returns contents of the file"""
+    
+    with open("spamtext.txt") as f:
+        text: str = f.read().split()
+    return text
 
-# The program starts after start seconds, to switch to another app
-time.sleep(start)
 
-with open("spamtext.txt") as f:
-    text: str = f.read().split()
+def main() -> None:
+    """__main__ function"""
+    
+    start: int = int(input("Enter the starting countdown: "))
+    interval: int = int(input("Enter the interval between each word: "))
 
-for word in text:
-    pyautogui.typewrite(word)
-    pyautogui.press("enter", interval=0) # interval between each word
+    # The program starts after start seconds, to switch to another app
+    time.sleep(start)
+
+    for word in text:
+        pyautogui.typewrite(word)
+        pyautogui.press("enter", interval=0) # interval between each word
+
+
+if __name__ == "__main__":
+    main()
